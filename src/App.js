c485ad7031a45105271427e3726c4c4a9951488b
rguzman <rguzman@viva.com.do>
2019-11-10 02:40:25 -0400

    Setting React Router and Trying to redirect when submit the form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Center from './components/Center';
+import Prueba from './components/Prueba';
+
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 function App() {
   return (
-   <Center />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Center} />
+        <Route exact path="/prueba" component={Prueba} />
+      </Switch>
+    </Router>
+  //  <Center />
   ); 
 }
 
--- src/components/Center.js
+++ src/components/Center.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+//import Prueba from './components/Prueba';
+
 
 export default function Center(props) {
   const [name, setName] = useState('');
+  const [data, setData] = useState();
 
   function handleNameChange(e) {
     setName(e.target.value);
@@ -15,20 +19,40 @@ export default function Center(props) {
       console.log(response);
       return response.json();
     }).then((myJson) => {
-      console.log(myJson);
+     // console.log(myJson.items);
+     setData(myJson.items);
+      //sleep(5000);
     });
+    console.log({data});
+    // Center.push("/prueba");
+    // <Link to="/prueba"></Link>;
   }
 
 
-  return (
+  //Function to sleep
+  // function sleep(milliseconds) {
+  //   let timeStart = new Date().getTime();
+  //   while (true) {
+  //     let elapsedTime = new Date().getTime() - timeStart;
+  //     if (elapsedTime > milliseconds) {
+  //       break;
+  //     }
+  //   }
+  // } 
 
+
+  return (
+    
     <form onSubmit={handleFecth}>
       <label>
         Repository:
         <input type="text" name="name" value={name} onChange={handleNameChange} />
       </label>
+      {console.log(data)}
       <input type="submit" value="Submit" />
+      {/* <Link to="/prueba"><input type="submit" value="Submit" /></Link> */}
     </form>
+
   );
 }
 
