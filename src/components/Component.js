9a12c0002dc01685ef0a68f725ccfae5623e2b50
rguzman <rguzman@viva.com.do>
2019-12-15 02:47:00 -0400

    Working with count to fetch more contributors

--- src/components/Component.js
+++ src/components/Component.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 export default function Component(props){
     const [contributors, setContributors] = useState( [] );
+    const [count, setCount] = useState(5);
     // const data;
     const  urlConstributors  =  props.location.state.data.contributors_url;
-    
+    const arrayMyJson = [];
 
     // function handleConstriborators(){
         useEffect(() => {
@@ -13,12 +14,39 @@ export default function Component(props){
                 return response.json();
                 // return <h1>Algo que poner</h1>;
               }).then((myJson) => {
+                
                 // console.log(myJson);
-                console.log(myJson);
-                setContributors(myJson);
+                // console.log(myJson[1]);
+                for (var i=0; i<count; i++){
+                  arrayMyJson.push(myJson[i]);
+                  // setContributors(myJson[i]);
+                }
+                setContributors(arrayMyJson);
+                console.log(arrayMyJson); 
 
             });
-          }, []);
+        }, []);
+
+        function CountAndFetch(){
+          setCount(count+5);
+        //   fetch(urlConstributors).then((response) => {
+        //     // console.log(response);
+        //     return response.json();
+        //     // return <h1>Algo que poner</h1>;
+        //   }).then((myJson) => {
+            
+        //     // console.log(myJson);
+        //     // console.log(myJson[1]);
+        //     for (var i=0; i<count; i++){
+        //       arrayMyJson.push(myJson[i]);
+        //       // setContributors(myJson[i]);
+        //     }
+        //     setContributors(arrayMyJson);
+        //     console.log(arrayMyJson); 
+
+        // });
+        // this.forceUpdate();
+        }
 
     // }
     return (
@@ -38,6 +66,7 @@ export default function Component(props){
       // </div>
         <div>
 
+          {/* {console.log(contributors)} */}
           {contributors.map(item => (
             <ul>
             <li key={item.id}>
@@ -54,6 +83,8 @@ export default function Component(props){
             </li>
             </ul>
           ))}
+            <button onClick={CountAndFetch}> + Load More</button>
+            {console.log(count)}
         </div>
         
       );
